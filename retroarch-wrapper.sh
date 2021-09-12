2b2123a4a2777b678f508958d1335bb6c9e055bd
Jason Tomsic <jrtomsic@gmail.com>
2021-09-11 22:57:36 -0400

    Create retroarch-wrapper.sh

--- retroarch-wrapper.sh
+++ retroarch-wrapper.sh
@@ -1,20 +1,2 @@
 #!/bin/sh
-
-rm -f /media/data/local/bin/*_libretro*.sh
-
-if [ "$1" = "--clean" ]; then
-  exit 0
-fi
-
-chmod 777 /media/data/local/bin/retroarch_rg350_odbeta
-
-for corepath in /media/data/local/home/.retroarch/cores/*.so; do
-  core=`basename ${corepath}`
-
-  filename="/media/data/local/bin/${core%.*}.sh"
-
-  echo "#!/bin/sh" > ${filename}
-  echo "/media/data/local/bin/retroarch_rg350_odbeta -L /media/data/local/home/.retroarch/cores/${core} --config /media/data/local/home/.retroarch/retroarch.cfg \"\${1}\"" >> ${filename}
-
-  chmod 777 ${filename}
-done
+/media/data/local/bin/retroarch_rg350_odbeta -L /media/data/local/home/.retroarch/cores/${1}_libretro.so --config /media/data/local/home/.retroarch/retroarch.cfg "${2}"
