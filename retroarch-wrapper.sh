0a3c673bf4862707cf6ae96184edf32f9c054ddb
Jason Tomsic <jrtomsic@gmail.com>
2021-09-11 23:02:07 -0400

    Update retroarch-wrapper.sh

--- retroarch-wrapper.sh
+++ retroarch-wrapper.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/media/data/local/bin/retroarch_rg350_odbeta -L /media/data/local/home/.retroarch/cores/${1}_libretro.so --config /media/data/local/home/.retroarch/retroarch.cfg "${2}"
+
+/media/data/local/bin/retroarch_rg350_odbeta \
+  -L /media/data/local/home/.retroarch/cores/${1}_libretro.so \
+  --config /media/data/local/home/.retroarch/retroarch.cfg \
+  "${2}"
